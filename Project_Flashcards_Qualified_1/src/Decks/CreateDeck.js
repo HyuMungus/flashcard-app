44035fd07e8ff75b6428437eb6b90771b8c0e94b
HyuMungus <33383578+HyuMungus@users.noreply.github.com>
2021-07-23 20:14:51 -0400

    Update CreateDeck.js

--- Project_Flashcards_Qualified_1/src/Decks/CreateDeck.js
+++ Project_Flashcards_Qualified_1/src/Decks/CreateDeck.js
@@ -14,8 +14,8 @@ export default function CreateDeck({ cancelHandler }) {
   function newDeckHandler(event) {
     event.preventDefault();
     async function createdDeck() {
-      const newDeck = await createDeck(deck);
-      setDeck({
+      const newDeck = await createDeck(deck);                     //creates new deck then sets deck back to initial state and redirects you to the page in which its located
+      setDeck({   
         id: 0,
         name: "",
         description: "",
@@ -28,7 +28,7 @@ export default function CreateDeck({ cancelHandler }) {
 
   function changeName(event) {
     setDeck({ ...deck, name: event.target.value });
-  }
+  }                                                               //updates values in form
   function changeDesc(event) {
     setDeck({ ...deck, description: event.target.value });
   }
