8d35b5b8f29aa92ade7f4e2cffb7b08a024460e6
HyuMungus <33383578+HyuMungus@users.noreply.github.com>
2021-07-23 20:18:53 -0400

    Update Home.js

--- Project_Flashcards_Qualified_1/src/Layout/Home.js
+++ Project_Flashcards_Qualified_1/src/Layout/Home.js
@@ -7,13 +7,13 @@ export default function Home({decks}) {
 
   async function deleteButtonHandler(deckId) {
     if (
-      window.confirm("Delete this deck? You will not be able to recover it.")
+      window.confirm("Delete this deck? You will not be able to recover it.")         //handles the deleting of decks then refreshes page
     ) {
       await deleteDeck(deckId);
       history.go(0);
     }
   }
-  const allDecks = decks.map((deck, index) => (
+  const allDecks = decks.map((deck, index) => (                 //maps all decks to be displayed on the screen
     <div className="card" key={index}>
       <div className="card-body">
         <h5 className="card-title">{deck.name}</h5>
